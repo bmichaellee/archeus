9107d35dad79aa53548abe237cac624196b6a435
Brandon Williamson <brandonmichaelwilliamson@outlook.com>
2020-12-13 04:12:25 -0500

    Add autoplay

--- ui/src/Components/ListenNow/SoundCloudEmbed.js
+++ ui/src/Components/ListenNow/SoundCloudEmbed.js
@@ -4,6 +4,6 @@ export const SoundCloudEmbed = () => (
     scrolling="no"
     frameBorder="no"
     allow="autoplay"
-    src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1177752979%3Fsecret_token%3Ds-2mQTzx1dVzm&color=%2300aabb&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+    src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1177752979%3Fsecret_token%3Ds-2mQTzx1dVzm&color=%2300aabb&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
   />
 )
